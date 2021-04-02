22b835016bff74ad4cf29f3de8b13c234212fe05
sai krishna <sai krishna@192.168.100.7>
2021-04-02 19:05:37 +0530

    Teaxj

--- CoreJava/src/com/core/java/Test1.java
+++ CoreJava/src/com/core/java/Test1.java
@@ -4,7 +4,7 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Testing-->1");
+System.out.println("Testing-->2");
 	}
 
 }
